60f2d6630a3026194f05e8639a95234100979b0e
Ohad Shaked <ohad.saked@gmail.com>
2023-01-19 14:54:14 +0200

    completed store.h & store.cpp, and checked with the given tester.

--- HW_week10_OhadShaked/HW_week10_OhadShaked/Store.cpp
+++ HW_week10_OhadShaked/HW_week10_OhadShaked/Store.cpp
@@ -2,6 +2,83 @@
 #include <fstream>
 #include <sstream>
 
+Store::Store(string storeName, string inventoryFileName)
+{
+	this->_storeName = storeName;
+	getInventoryFromFile(inventoryFileName);
+}
+
+string Store::getName() const
+{
+	return this->_storeName;
+}
+
+std::ostream& operator<<(std::ostream& os, const Store& store)
+{
+	os << "Store Name: " + store._storeName + "\nProducts:\n";
+	for (int i = 0; i < store._products.size(); i++)
+	{
+		os << "[" + std::to_string(i) + "] - ";
+		os << store[i];
+	}
+	return os;
+}
+
+string Store::getProductListFilteredByCategory(ItemCategory category)
+{
+	string retStr;
+	for (int i = 0; i < this->_products.size(); i++)
+	{
+		if (this->_products[i].getCategory() == category)
+		{
+			retStr += "[Serial: " + this->_products[i].getSerial() + ", Name: " + this->_products[i].getName() + ", Category: " + getItemCategoryString(this->_products[i].getCategory()) + ", Price: " + std::to_string(this->_products[i].getPrice()) + ", Amount: " + std::to_string(this->_products[i].getCount()) + "]\n";
+		}
+	}
+	return retStr;
+}
+
+string Store::getProductsStr()
+{
+	string retStr;
+	for (int i = 0; i < this->_products.size(); i++)
+	{
+		retStr += "[Serial: " + this->_products[i].getSerial() + ", Name: " + this->_products[i].getName() + ", Category: " + getItemCategoryString(this->_products[i].getCategory()) + ", Price: " + std::to_string(this->_products[i].getPrice()) + ", Amount: " + std::to_string(this->_products[i].getCount()) + "]\n";
+	}
+	return retStr;
+}
+
+string Store::getSortedProductList(SortingCriteria sortingCriteria)
+{
+	if (sortingCriteria == SERIAL)
+	{
+		sort(this->_products.begin(), this->_products.end(), SerialComparator);
+	}
+	if (sortingCriteria == NAME)
+	{
+		sort(this->_products.begin(), this->_products.end(), NameComparator);
+	}
+	if (sortingCriteria == PRICE)
+	{
+		sort(this->_products.begin(), this->_products.end(), PriceComparator);
+	}
+	if (sortingCriteria == CATEGORY)
+	{
+		sort(this->_products.begin(), this->_products.end(), CategoryComparator);
+	}
+	return this->getProductsStr();
+}
+
+Item Store::operator[](const int itemNumber) const
+{
+	if (itemNumber < this->_products.size() && itemNumber >= 0)
+	{
+		return this->_products[itemNumber];
+	}
+	else
+	{
+		throw std::invalid_argument("ERROR: index was out of the deque's boundarys.\n");
+	}
+}
 void Store::getInventoryFromFile(const std::string inventoryFileName)
 {
 	// This function reads a CSV file into the object inventory (std::deque<Item>)
--- HW_week10_OhadShaked/HW_week10_OhadShaked/Store.h
+++ HW_week10_OhadShaked/HW_week10_OhadShaked/Store.h
@@ -2,7 +2,12 @@
 #include <string>
 #include <iostream>
 #include <deque>
+#include<algorithm>
 #include "Item.h"
+
+
+using std::string;
+
 
 /////////////////////////////
 // Comparators for Sorting //
@@ -21,7 +26,7 @@ struct {
 struct {
 	bool operator()(const Item& a, const Item& b)
 	{
-		return 	/* Complete by yourself */
+		return 	a.getName() < b.getName();
 	}
 } NameComparator;
 
@@ -31,6 +36,13 @@ struct {
 		return a.getPrice() < b.getPrice();
 	}
 } PriceComparator;
+
+struct {
+	bool operator()(const Item& a, const Item& b)
+	{
+		return 	a.getSerial() < b.getSerial();
+	}
+} SerialComparator;
 
 
 enum SortingCriteria {CATEGORY = 0, NAME = 1, PRICE = 2, SERIAL = 3};
@@ -43,44 +55,25 @@ class Store
 {
 public:
 
-	/////////////////
-    // Constructor //
-    /////////////////
+	Store(string storeName, string inventoryFileName);
 
-	/* Complete by yourself */
+	string getName() const;
 
-	/////////////////////////
-    // getters and setters //
-    /////////////////////////
-
-	/* Complete by yourself */
-
-	/////////////
-    // methods //
-    /////////////
-
-	/* Complete by yourself */
-
-    ///////////////
-    // operators //
-    ///////////////
+	string getSortedProductList(SortingCriteria sortingCriteria);
+	string getProductListFilteredByCategory(ItemCategory category);
 
 	// Element access operator for the store's items
 	Item operator[](const int itemNumber) const;
 
-    // operator << for easy printing, prints the name of the store and the products
+	// operator << for easy printing, prints the name of the store and the products
 	// with their indexes
 	friend std::ostream& operator<<(std::ostream& os, const Store& store);
 
 private:
 
-    // Helper Method, reads a '.csv' file and adding the items to the products list.
+	// Helper Method, reads a '.csv' file and adding the items to the products list.
 	void getInventoryFromFile(const std::string inventoryFileName);
-
-    ////////////
-    // Fields //
-    ////////////
-
+	string getProductsStr();
 	std::string _storeName;
 	std::deque<Item> _products;
 };
